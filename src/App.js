6966d3990bca432a33ef1d3e540634e4c3b7b8e7
Vikram Singh <vik.singh.2019@gmail.com>
2021-01-19 17:02:03 -0500

    Update data and visualization component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import * as d3 from "d3";
+import { data } from "./data/data.js";
+
+import { dummyPosts, dummyRelations } from "./data/postDummyData.js";
 
 function App() {
+  const svgRef = useRef();
+
+  const width = 800;
+  const height = 800;
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const color = d3.scaleSequential([8, 0], d3.interpolateMagma);
+    const format = d3.format(",d");
+
+    var pack = (data) =>
+      d3
+        .pack()
+        .size([width - 2, height - 2])
+        .padding(3)(
+        d3
+          .hierarchy(data)
+          .sum((d) => d.value)
+          .sort((a, b) => b.value - a.value)
+      );
+
+    const svg = d3.select(svgRef.current);
+    const root = pack(formatData(dummyPosts, dummyRelations));
+
+    svg
+      .attr("viewBox", [0, 0, width, height])
+      .style("font", "10px sans-serif")
+      .attr("text-anchor", "middle");
+
+    const node = svg
+      .selectAll("g")
+      .data(d3.group(root.descendants(), (d) => d.height))
+      .join("g")
+      .selectAll("g")
+      .data((d) => d[1])
+      .join("g")
+      .attr("transform", (d) => `translate(${d.x + 1},${d.y + 1})`);
+
+    node
+      .append("circle")
+      .attr("r", (d) => d.r)
+      .attr("fill", (d) => color(d.height));
+
+    const leaf = node.filter((d) => !d.children);
+
+    leaf
+      .append("text")
+      .attr("clip-path", (d) => d.clipUid)
+      .selectAll("tspan")
+      .data((d) => d.data.title.split(/(?=[A-Z][a-z])|\s+/g))
+      .join("tspan")
+      .attr("x", 0)
+      .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
+      .text((d) => d);
+
+    node.append("title").text(
+      (d) =>
+        `${d
+          .ancestors()
+          .map((d) => d.data.title)
+          .reverse()
+          .join("/")}\n${format(d.value)}`
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+    </React.Fragment>
   );
+}
+
+function formatData(posts, relations) {
+  //Array containing the root objects (the parents)
+  let roots = [];
+
+  //map to get posts by id {postID, postObject}
+  let postsMap = new Map();
+
+  //map each post by ID
+  posts.forEach((post) => {
+    //give each post object a children array
+    post.children = [];
+    post.value = Math.floor(Math.random() * 100);
+
+    //map each post by its ID
+    postsMap.set(post._id, post);
+
+    //fill the roots array with all posts
+    roots.push(post);
+  });
+
+  for (let i = 0; i < relations.length; i++) {
+    let parent = postsMap.get(relations[i].post1);
+    let child = postsMap.get(relations[i].post2);
+
+    parent.children.push(child);
+    if (roots.includes(child)) roots.splice(roots.indexOf(child), 1);
+  }
+
+  return roots.length === 1 ? roots[0] : { value: 1, children: roots };
 }
 
 export default App;
---
+++ src/data/postDummyData.js
@@ -0,0 +1,354 @@
+export const dummyPosts = [
+  {
+    _id: "5f85b65d48bb5c021b8f68h8",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Hello World",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 1",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+  {
+    _id: "5f85b65d48bb5c021b8f68a8",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Hello World",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 1",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+  {
+    _id: "5f85b65d48bb5c021b8f68a2",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Hello World",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 1",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+  {
+    _id: "5f85b65d48bb5c021b8f68d8",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Hello World",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 1",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+  {
+    _id: "5f85b65d48bb5c021b8f68d6",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Child Post 2",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 2",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+
+  {
+    _id: "5f85b65d48bb5c021b8f68d9",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Child Post 3",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 3",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+
+  {
+    _id: "5f85b65d48bb5c021b8f68d2",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Child of Child 1",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 3",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+
+  {
+    _id: "5f85b65d48bb5c021b8f68d3",
+    owner: [
+      {
+        $oid: "5f8537bb48bb5c021b8f68cd",
+      },
+    ],
+    followers: [],
+    votes: [],
+    tags: [],
+    title: "Child of Child 2",
+    type: "Topic",
+    public: true,
+    anon: false,
+    body: "This is sample post 3",
+    poll: [],
+    qa: [],
+    tasks: [],
+    comments: [],
+    createdAt: {
+      $date: {
+        $numberLong: "1602598493431",
+      },
+    },
+    updatedAt: {
+      $date: {
+        $numberLong: "1603216450342",
+      },
+    },
+    __v: {
+      $numberInt: "32",
+    },
+    edited: true,
+  },
+];
+
+export const dummyRelations = [
+  {
+    _id: {
+      $oid: "5f85c73748bb5c021b8f68e7",
+    },
+    post2: "5f85b65d48bb5c021b8f68a8",
+    type: "parent-child",
+    post1: "5f85b65d48bb5c021b8f68a2",
+    owner: {
+      $oid: "5f85bde948bb5c021b8f68e0",
+    },
+    __v: {
+      $numberInt: "0",
+    },
+  },
+
+  {
+    _id: {
+      $oid: "5f85c73748bb5c021b8f68e7",
+    },
+    post2: "5f85b65d48bb5c021b8f68d6",
+    type: "parent-child",
+    post1: "5f85b65d48bb5c021b8f68d8",
+    owner: {
+      $oid: "5f85bde948bb5c021b8f68e0",
+    },
+    __v: {
+      $numberInt: "0",
+    },
+  },
+
+  {
+    _id: {
+      $oid: "5f85c73748bb5c021b8f68e",
+    },
+    post2: "5f85b65d48bb5c021b8f68d9",
+    type: "parent-child",
+    post1: "5f85b65d48bb5c021b8f68d8",
+    owner: {
+      $oid: "5f85bde948bb5c021b8f68e0",
+    },
+    __v: {
+      $numberInt: "0",
+    },
+  },
+
+  {
+    _id: {
+      $oid: "5f85c73748bb5c021b8f68e",
+    },
+    post2: "5f85b65d48bb5c021b8f68d3",
+    type: "parent-child",
+    post1: "5f85b65d48bb5c021b8f68d9",
+    owner: {
+      $oid: "5f85bde948bb5c021b8f68e0",
+    },
+    __v: {
+      $numberInt: "0",
+    },
+  },
+
+  {
+    _id: {
+      $oid: "5f85c73748bb5c021b8f68e",
+    },
+    post2: "5f85b65d48bb5c021b8f68d2",
+    type: "parent-child",
+    post1: "5f85b65d48bb5c021b8f68d9",
+    owner: {
+      $oid: "5f85bde948bb5c021b8f68e0",
+    },
+    __v: {
+      $numberInt: "0",
+    },
+  },
+];
